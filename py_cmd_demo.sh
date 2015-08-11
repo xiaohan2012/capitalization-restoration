309d4e229d9a969880e2a0851c16a375cef89c57
Han Xiao <han.xiao@cs.helsinki.fi>
2015-08-11 17:27:28 +0300

    modifyed demo script

--- py_cmd_demo.sh
+++ py_cmd_demo.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+data=$(cat data/example_data.json)
 
-python capitalization_restoration_web.py --json_string '{"capitalizedSentences": [{"no":33,"tokens":["#","#","#"],"pos":[null,null,null]},{"no":22,"tokens":["About","the","Author"],"pos":["p","det","n"]}], "otherSentences": []}'
+python capitalization_restoration_web.py --json_string ${data}
+
 echo -e "\n"
