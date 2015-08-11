9d7eed895046e5c045b208a4ed094148bda89f3f
Han Xiao <han.xiao@cs.helsinki.fi>
2015-08-11 17:13:33 +0300

    service: new impl on the new input + tests added

---
+++ service.py
@@ -0,0 +1,125 @@
+import json
+import tornado.ioloop
+import tornado.web
+
+import nltk
+nltk.data.path.append('nltk_data')
+
+from cap_restore import DefaultRestorer
+
+import traceback
+
+restorer = DefaultRestorer()
+
+
+STATUS_RESTORE_ERROR = -2
+STATUS_INVALID_PARAM = -1
+STATUS_OK = 0
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def write_error_msg(self, code, msg):
+        self.write({"code": code, "msg": msg})
+
+    def valid_data(self, data):
+        fields = ['capitalizedSentences', 'otherSentences']
+        for field in fields:
+            if field not in data:
+                return False, 'd: {} not present'.format(field)
+        
+        for field in fields:
+            if not isinstance(data[field], list):
+                return False, 'd: {} not list'.format(field)
+
+        for field in fields:
+            for sent in data[field]:
+                flag, msg = self.valid_sent(sent)
+                if not flag:
+                    return False, msg
+
+        return True, ''
+            
+    def valid_sent(self, sent):
+        fields = ['no', 'tokens', 'pos']
+
+        for f in fields:
+            if f not in sent:
+                return False, 's: {} not present.'.format(f)
+        
+        # token checking
+        if not isinstance(sent['tokens'], list):
+            return False, 'tokens: {} not list. {}'.format(
+                sent['tokens'], sent
+            )
+        for tok in sent['tokens']:
+            if not isinstance(tok, basestring):
+                return False, 'tok: {} not string'.format(tok)
+
+        # pos checking
+        if not isinstance(sent['pos'], list):
+            return False, 'pos: {} not list. {}'.format(sent['pos'], sent)
+
+        for p in sent['pos']:
+            if not (isinstance(p, basestring)
+                    or p == None):
+                return False, 'pos: {} not string'.format(p)
+
+        return True, ''
+
+    def post(self):
+        """
+        Request parameter:
+        ----------------------
+        
+        json_string: string
+            the dict(after parsing) should contain the following fields:
+            - 'capitalizedSentences': list<dict<no, tokens, pos>>, one dict one sent
+            - 'otherSentences': same format as above
+
+        """
+        try:
+            data = json.loads(self.request.body)
+        except ValueError:
+            self.write_error_msg(STATUS_INVALID_PARAM,
+                                 msg="JSON decode error")
+            return
+        valid, msg = self.valid_data(data)
+        if not valid:
+            self.write_error_msg(STATUS_INVALID_PARAM,
+                                 msg=msg)
+            return
+            
+        try:
+            ans = {}
+            ans['result'] = []
+            ans['code'] = STATUS_OK
+            
+            doc = [sent['tokens'] for sent in data['otherSentences']]
+            for r in data['capitalizedSentences']:
+                decap_tokens = restorer.restore(r['tokens'],
+                                                pos=r['pos'],
+                                                doc=doc)
+                ans['result'].append({'no': r['no'], 'tokens': decap_tokens})
+            self.write(ans)
+
+        except:
+            if self.settings.get("debug"):
+                self.write_error_msg(STATUS_RESTORE_ERROR,
+                                     msg=traceback.format_exc())
+            else:
+                self.write_error_msg(STATUS_RESTORE_ERROR,
+                                     msg="Restoration error occurred")
+
+settings = {
+    'debug': True,
+}
+
+handlers = [
+    (r"/caprestore", MainHandler),
+]
+
+application = tornado.web.Application(handlers, **settings)
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
---
+++ tests/test_service_http.py
@@ -0,0 +1,77 @@
+import os
+import json
+import urllib2
+from pathlib import Path
+
+from nose.tools import assert_equal
+
+from capitalization_restoration.service import (STATUS_RESTORE_ERROR,
+                                                STATUS_INVALID_PARAM,
+                                                STATUS_OK)
+
+CURDIR = os.path.dirname(os.path.realpath(__file__))
+
+def make_err_msg(code, msg):
+    return {"code": code, "msg": msg}
+
+
+def make_request(data):
+    req = urllib2.urlopen(
+        "http://{}:{}/caprestore".format('localhost', '8888'),
+        json.dumps(data)
+    )
+    return json.loads(req.read())
+
+
+def make_json_data(cap_sents=[], other_sents=[]):
+    return {'capitalizedSentences': cap_sents,
+            'otherSentences': other_sents}
+
+
+def test_missing_fields():
+    # 'capitalizedSentences' shoudl be present
+    assert_equal(
+        make_request({'haha': None})['code'],
+        STATUS_INVALID_PARAM
+    )
+    
+    # 'otherSentences' shoudl be present
+    assert_equal(
+        make_request({'capitalizedSentences': None})['code'],
+        STATUS_INVALID_PARAM
+    )
+
+
+invalid_params = [
+    # missing pos, tokens and no for each case
+    [{'no': 1, 'tokens': ['a', 'b']}],
+    [{'no': 1, 'pos': ['a', 'b']}],
+    [{'tokens': ['a', 'b'], 'pos': ['a', 'b']}],
+    # not a list
+    [{'tokens': ['a', 'b'], 'pos': 'a'}],
+    # not a list of str
+    [{'tokens': ['a', 'b', 1], 'pos': ['a']}]
+]
+
+    
+def test_invalid_type():
+    for param in invalid_params:
+        for arg_name in ['cap_sents', 'other_sents']:
+            kwargs = {arg_name: param}
+            assert_equal(
+                make_request(make_json_data(**kwargs))['code'],
+                STATUS_INVALID_PARAM
+            )
+    
+
+def test_success_case():
+    with Path(CURDIR, 'data/request_data.json').open(encoding='utf8') as f:
+        req_data = json.loads(f.read())
+        res = make_request(req_data)
+        print res
+        assert_equal(res['code'], STATUS_OK)
+        assert_equal(res['result'][:2], [
+            {'no': 33, 'tokens': u'# # #'.split()},
+            {'no': 22, 'tokens': u'About the author'.split()},
+        ])
+        assert_equal(len(res['result']), 9)
