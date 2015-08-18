9fa861b6bdca864c7ce809b9d037312f310ed13b
Han Xiao <han.xiao@cs.helsinki.fi>
2015-08-18 14:50:06 +0300

    test.data.py: added china example

--- tests/data.py
+++ tests/data.py
@@ -7,20 +7,29 @@ from toolz.dicttoolz import get_in
 CURDIR = os.path.dirname(os.path.realpath(__file__))
 
 get_tokens = partial(map, partial(get_in, ['token']))
+get_lemmas = partial(map, partial(get_in, ['lemma']))
 get_tags = partial(map, partial(get_in, ['pos']))
 
 
-def load_turkish_example():
+def load_example_by_id(id_):
     # data preparation
-    with Path(CURDIR + '/data/BBCEC51D429DD4B06739107B121D69B7.title').open(encoding='utf8') as title_f, \
-         Path(CURDIR + '/data/BBCEC51D429DD4B06739107B121D69B7.doc').open(encoding='utf8') as doc_f:
+    with Path(CURDIR + '/data/{}.title'.format(id_)).open(encoding='utf8') as title_f, \
+         Path(CURDIR + '/data/{}.doc'.format(id_)).open(encoding='utf8') as doc_f:
         title_raw = json.loads(title_f.read())
         doc_raw = json.loads(doc_f.read())
         doc = [[doc['token'] for doc in sent['features']]
                for sent in doc_raw]
 
     return (get_tokens(title_raw),
+            get_lemmas(title_raw),
             get_tags(title_raw),
             doc)
 
+    
+def load_turkish_example():
+    return load_example_by_id('BBCEC51D429DD4B06739107B121D69B7')
 
+
+def load_china_example():
+    return load_example_by_id('f4')
+    
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,11 +1,15 @@
 from nose.tools import assert_equal
-from data import load_turkish_example
+from data import load_example_by_id
 
 
-def test_load_turkish_example():
-    toks, tags, doc = load_turkish_example()
+def test_load_example_by_id():
+    toks, lemmas, tags, doc = load_example_by_id(
+        'BBCEC51D429DD4B06739107B121D69B7'
+    )
     assert_equal(toks, ['New', 'Opportunities', 'for', 'Lithuanian',
                         'and', 'Turkish', 'Business'])
+    assert_equal(lemmas, [u'new', u'opportunity', u'for', u'lithuanian',
+                          u'', u'turkish', u'business'])
     assert_equal(tags, ['adj', 'n', 'p', 'adj', None, 'adj', 'n'])
     assert_equal(doc, [[u'Three', u'agreements', u'will', u'be', u'signed',
                         u'with', u'a', u'view', u'to', u'strengthening',
@@ -21,4 +25,9 @@ def test_load_turkish_example():
                         u'up', u'new', u'opportunities', u'for', u'their',
                         u'business', u'people', u'.'],
                        [u'Our', u'countries', u'are', u'\u2026']
-    ])
+                   ])
+    toks, lemmas, tags, doc = load_example_by_id(
+        'f4'
+    )
+    assert_equal(len(toks), 10)
+    assert_equal(toks[0], 'China')
