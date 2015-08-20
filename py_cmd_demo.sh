f47f42168aff471715c7945971ae02c56cd1891e
Han Xiao <han.xiao@cs.helsinki.fi>
2015-08-20 15:32:35 +0300

    added more request demos

--- py_cmd_demo.sh
+++ py_cmd_demo.sh
@@ -1,7 +1,20 @@
 #! /bin/bash
 
-data=$(cat data/example_data.json)
+for path in data/request_example_data/*.json; do
+	echo $path
+	data=$(cat $path)
+	python capitalization_restoration_web.py --json_string "${data}"
+	echo -e "\n"
+done
 
-python capitalization_restoration_web.py --json_string ${data}
+# # data=$(cat data/request_example_data/example_data.json)
+# # # data=$(cat data/request_example_data/6788E2B8AE48C327A33DA66CCE962ADE.json)
+# # # data=$(cat data/request_example_data/199C8BB69F17FEA871D45CC08B43CE91.json)
+# # data=$(cat data/request_example_data/8AC66B1DCD4E138D49B36BE6DB6019E3.json)
+# # # data=$(cat data/request_example_data/85D853A8165AE3E8C312BBCF3B8B8EFC.json)
+# # # data=$(cat data/request_example_data/4ABBA038CB08B29A0EE204DFEE9C7660.json)
+# # # data=$(cat data/request_example_data/89F45109E7576F0AD4FE40095C8AD0D4.json)
 
-echo -e "\n"
+# python capitalization_restoration_web.py --json_string "${data}"
+
+# echo -e "\n"
